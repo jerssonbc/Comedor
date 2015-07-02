c145fcb523c45f301c29552d006a696d8fa89814
Jersson Bacilio <jerss.edu@gmail.com>
2015-07-02 02:17:27 -0500

    Actualizando base

--- sql/dbcomedor.sql
+++ sql/dbcomedor.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-06-2015 a las 16:37:38
+-- Tiempo de generación: 02-07-2015 a las 09:15:37
 -- Versión del servidor: 5.6.17
 -- Versión de PHP: 5.5.12
 
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `comensales` (
   KEY `comensales_institucion_id_foreign` (`institucion_id`),
   KEY `comensales_tipocomensal_id_foreign` (`tipocomensal_id`),
   KEY `comensales_programa_id_foreign` (`programa_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=39 ;
 
 --
 -- Volcado de datos para la tabla `comensales`
@@ -113,7 +113,17 @@ INSERT INTO `comensales` (`id`, `dni`, `ape_paterno`, `ape_maerno`, `nombre`, `c
 (21, '70665521', 'bac', 'jers', 'jejej', '1000000011', 1, 3, NULL, NULL, NULL, 3, '2015-06-08', 1, '2015-06-08 05:00:00', '2015-06-08 05:00:00'),
 (22, '70665529', 'bac', 'jers', 'jdfj', '1000000012', 1, 3, NULL, NULL, NULL, 3, '2015-06-08', 1, '2015-06-08 05:00:00', '2015-06-08 05:00:00'),
 (23, '74859632', 'Altamirano', 'Guevara', 'Neyder', '1000000013', 1, 3, NULL, NULL, NULL, 3, '2015-06-08', 1, '2015-06-08 05:00:00', '2015-06-08 05:00:00'),
-(24, '19032574', 'Vega', 'Santos', 'jOrge', '1000000012', 1, 3, NULL, NULL, NULL, 3, '2015-06-17', 1, '2015-06-17 05:00:00', '2015-06-17 05:00:00');
+(24, '19032574', 'Vega', 'Santos', 'jOrge', '1000000012', 1, 3, NULL, NULL, NULL, 3, '2015-06-17', 1, '2015-06-17 05:00:00', '2015-06-17 05:00:00'),
+(29, '19033605', 'Carmona', 'Gutierez', 'Jaqueline', '1000000014', 1, 3, NULL, NULL, NULL, 3, '2015-06-29', 1, '2015-06-29 05:00:00', '2015-06-29 05:00:00'),
+(30, '17033305', 'Zavaleta', 'Bacilio', 'Jefersson', '1000000015', 1, 3, NULL, NULL, NULL, 3, '2015-06-29', 1, '2015-06-29 05:00:00', '2015-06-29 05:00:00'),
+(31, '19253684', 'Cabrera', 'Gonzales', 'Roberto', '1000000016', 2, 3, NULL, NULL, NULL, 3, '2015-06-29', 1, '2015-06-29 05:00:00', '2015-06-29 05:00:00'),
+(32, '19023645', 'Guzman', 'Pasco', 'Fabi', '1000000017', 1, 3, NULL, NULL, NULL, 3, '2015-06-30', 1, '2015-06-30 05:00:00', '2015-06-30 05:00:00'),
+(33, '19023665', 'Montoya', 'Rodriguez', 'Roxana', '1000000018', 1, 3, NULL, NULL, NULL, 3, '2015-06-30', 1, '2015-06-30 05:00:00', '2015-06-30 05:00:00'),
+(34, '70556389', 'Estrada', 'Iglesias', 'Cesar', '1000000019', 1, 3, NULL, NULL, NULL, 3, '2015-06-30', 1, '2015-06-30 05:00:00', '2015-06-30 05:00:00'),
+(35, '74856321', 'Guzman', 'Sanche', 'Karen', '1000000020', 1, 3, NULL, NULL, NULL, 3, '2015-06-30', 1, '2015-06-30 05:00:00', '2015-06-30 05:00:00'),
+(36, '78456321', 'Sanchez', 'Carranza', 'Karito', '1000000021', 1, 3, NULL, NULL, NULL, 3, '2015-06-30', 1, '2015-06-30 05:00:00', '2015-06-30 05:00:00'),
+(37, '74758410', 'Perez', 'Zavaleta', 'Jeniffer', '1000000022', 1, 3, NULL, NULL, NULL, 2, '2015-06-30', 1, '2015-06-30 05:00:00', '2015-06-30 05:00:00'),
+(38, '78192536', 'Ruiz', 'Vera', 'Geovani', '1000000023', 1, 3, NULL, NULL, NULL, 1, '2015-06-30', 1, '2015-06-30 05:00:00', '2015-06-30 05:00:00');
 
 -- --------------------------------------------------------
 
@@ -164,6 +174,55 @@ INSERT INTO `cronogramas_servicio` (`id`, `fecha`, `estado`, `comensal_id`) VALU
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `escuela`
+--
+
+CREATE TABLE IF NOT EXISTS `escuela` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(60) NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  `facultad_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `facultad_id` (`facultad_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+--
+-- Volcado de datos para la tabla `escuela`
+--
+
+INSERT INTO `escuela` (`id`, `descripcion`, `estado`, `facultad_id`) VALUES
+(1, 'Ingeniería Civil', 1, 1),
+(2, 'Ingeniería Industrial', 1, 1),
+(3, 'Ingeniería de Materiales', 1, 1),
+(4, 'Ingeniería Mecánica', 1, 1),
+(5, 'Ingeniería de Sisemas', 1, 1),
+(6, 'Educación Inicial', 1, 2),
+(7, 'Eduación Primaria', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `facultad`
+--
+
+CREATE TABLE IF NOT EXISTS `facultad` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(60) NOT NULL,
+  `estado` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `facultad`
+--
+
+INSERT INTO `facultad` (`id`, `descripcion`, `estado`) VALUES
+(1, 'Ingenieria', 1),
+(2, 'Educación y Ciencias de la Comunicación', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `instituciones`
 --
 
@@ -201,7 +260,7 @@ CREATE TABLE IF NOT EXISTS `menus` (
   `created_at` timestamp NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
 
 --
 -- Volcado de datos para la tabla `menus`
@@ -215,7 +274,8 @@ INSERT INTO `menus` (`id`, `padre`, `nombre`, `url`, `estado`, `created_at`, `up
 (5, 2, 'REGISTRAR ASISTENCIA', 'vista/registarAsistencia', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
 (6, 3, 'HISTORIAL POR ALUMNO', 'vista/historialAlumno', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
 (7, 2, 'Registro Comensal', 'vista/registrocomensal', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(8, 3, 'Comensales Registrados', 'vista/comensalesregistrados', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+(8, 3, 'Comensales Registrados', 'vista/comensalesregistrados', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(9, 2, 'PARAMETROS', 'vista/adminparametros', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -250,6 +310,23 @@ INSERT INTO `migrations` (`migration`, `batch`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `parametros`
+--
+
+CREATE TABLE IF NOT EXISTS `parametros` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `codigo` int(11) NOT NULL,
+  `anio` int(11) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_termino` date NOT NULL,
+  `estado` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `programas`
 --
 
@@ -270,6 +347,23 @@ INSERT INTO `programas` (`id`, `descripcion`, `estado`, `created_at`, `updated_a
 (1, 'BECA', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
 (2, 'MEDIA BECA', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
 (3, 'PROGRAMA A', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `regularizaciones`
+--
+
+CREATE TABLE IF NOT EXISTS `regularizaciones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comensal_id` int(11) NOT NULL,
+  `fecha_registro` date NOT NULL,
+  `estado` tinyint(1) NOT NULL,
+  `img_voucher` varchar(45) NOT NULL,
+  `img_ficham` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comensal_id` (`comensal_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -308,7 +402,7 @@ CREATE TABLE IF NOT EXISTS `rol_menu` (
   PRIMARY KEY (`id`),
   KEY `rol_menu_rol_id_foreign` (`rol_id`),
   KEY `rol_menu_menu_id_foreign` (`menu_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
 
 --
 -- Volcado de datos para la tabla `rol_menu`
@@ -324,7 +418,8 @@ INSERT INTO `rol_menu` (`id`, `rol_id`, `menu_id`) VALUES
 (7, 2, 5),
 (8, 1, 6),
 (9, 1, 7),
-(10, 1, 8);
+(10, 1, 8),
+(11, 1, 9);
 
 -- --------------------------------------------------------
 
@@ -346,9 +441,9 @@ CREATE TABLE IF NOT EXISTS `tipos_comensal` (
 --
 
 INSERT INTO `tipos_comensal` (`id`, `descripcion`, `estado`, `created_at`, `updated_at`) VALUES
-(1, 'Becado', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(2, 'docente', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(3, 'alumno', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+(1, 'Administrativo', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'Docente', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 'Alumno', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -405,6 +500,31 @@ INSERT INTO `turnos` (`id`, `descripcion`, `hora_inicio`, `hora_fin`, `estado`, 
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `universitario_unt`
+--
+
+CREATE TABLE IF NOT EXISTS `universitario_unt` (
+  `comensal_id` int(11) NOT NULL,
+  `escuela_id` int(11) NOT NULL,
+  `cod_matricula` char(10) NOT NULL,
+  PRIMARY KEY (`comensal_id`),
+  KEY `escuela_id` (`escuela_id`),
+  KEY `comensal_id` (`comensal_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `universitario_unt`
+--
+
+INSERT INTO `universitario_unt` (`comensal_id`, `escuela_id`, `cod_matricula`) VALUES
+(29, 5, ''),
+(30, 5, ''),
+(36, 5, ''),
+(37, 5, '');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -419,7 +539,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `imagen` varchar(50) DEFAULT NULL,
   `id_trabajador` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=28 ;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -441,7 +561,17 @@ INSERT INTO `usuarios` (`id`, `usuario`, `password`, `estado`, `created_at`, `up
 (14, 'sarate', 'd41d8cd98f00b204e9800998ecf8427e', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL, NULL, 1),
 (15, 'Luis', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 4, NULL, NULL),
 (16, 'geraldine', 'd41d8cd98f00b204e9800998ecf8427e', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL, NULL, 2),
-(17, 'jorge', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 22, NULL, NULL);
+(17, 'jorge', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 22, NULL, NULL),
+(18, 'jaqui', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 29, NULL, NULL),
+(19, 'jeferson', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 30, NULL, NULL),
+(20, 'roberto', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 31, NULL, NULL),
+(21, 'fabicita', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 32, NULL, NULL),
+(22, 'roxi', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 33, NULL, NULL),
+(23, 'cesar', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 34, NULL, NULL),
+(24, 'karen', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 35, NULL, NULL),
+(25, 'karito', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 36, NULL, NULL),
+(26, 'jeni', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 37, NULL, NULL),
+(27, 'geo', '202cb962ac59075b964b07152d234b70', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 38, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -456,7 +586,7 @@ CREATE TABLE IF NOT EXISTS `usuario_rol` (
   PRIMARY KEY (`id`),
   KEY `usuario_rol_usuario_id_foreign` (`usuario_id`),
   KEY `usuario_rol_rol_id_foreign` (`rol_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=25 ;
 
 --
 -- Volcado de datos para la tabla `usuario_rol`
@@ -475,7 +605,17 @@ INSERT INTO `usuario_rol` (`id`, `usuario_id`, `rol_id`) VALUES
 (11, 14, 1),
 (12, 15, 3),
 (13, 16, 2),
-(14, 17, 3);
+(14, 17, 3),
+(15, 18, 3),
+(16, 19, 3),
+(17, 20, 3),
+(18, 21, 3),
+(19, 22, 3),
+(20, 23, 3),
+(21, 24, 3),
+(22, 25, 3),
+(23, 26, 3),
+(24, 27, 3);
 
 --
 -- Restricciones para tablas volcadas
@@ -509,11 +649,23 @@ ALTER TABLE `cronogramas_servicio`
   ADD CONSTRAINT `cronogramas_servicio_comensal_id_foreign` FOREIGN KEY (`comensal_id`) REFERENCES `comensales` (`id`);
 
 --
+-- Filtros para la tabla `escuela`
+--
+ALTER TABLE `escuela`
+  ADD CONSTRAINT `escuela_ibfk_1` FOREIGN KEY (`facultad_id`) REFERENCES `facultad` (`id`);
+
+--
 -- Filtros para la tabla `rol_menu`
 --
 ALTER TABLE `rol_menu`
   ADD CONSTRAINT `rol_menu_menu_id_foreign` FOREIGN KEY (`menu_id`) REFERENCES `menus` (`id`),
   ADD CONSTRAINT `rol_menu_rol_id_foreign` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`);
+
+--
+-- Filtros para la tabla `universitario_unt`
+--
+ALTER TABLE `universitario_unt`
+  ADD CONSTRAINT `uunt_ecuela_id_foreign` FOREIGN KEY (`escuela_id`) REFERENCES `escuela` (`id`);
 
 --
 -- Filtros para la tabla `usuario_rol`
