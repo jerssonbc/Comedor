7727b92ae5df7c2a08d2ad54637a760eb99ba921
Jersson Bacilio <jerss.edu@gmail.com>
2015-07-10 15:19:18 -0500

    Cambios antes de final Presentación

--- Comedor/app/src/main/java/com/neyder/comedor/ListadoActivity.java
+++ Comedor/app/src/main/java/com/neyder/comedor/ListadoActivity.java
@@ -35,8 +35,11 @@ public class ListadoActivity extends ActionBarActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listar_comensales);
+<<<<<<< HEAD
         /*toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
         setSupportActionBar(toolbar);*/
+=======
+>>>>>>> 04883d5a1020b7f25fea68bd2a543e9dd270b8b3
 
         Thread tr = new Thread(){
             @Override
@@ -47,7 +50,10 @@ public class ListadoActivity extends ActionBarActivity{
                             @Override
                             public void run() {
                                 cargaListado(obtDatosJSON(Resultado));
+<<<<<<< HEAD
                                 //Toast.makeText(getApplicationContext(),Resultado,Toast.LENGTH_SHORT).show();
+=======
+>>>>>>> 04883d5a1020b7f25fea68bd2a543e9dd270b8b3
                             }
                         });
             }
--- android/asistencias.php
+++ android/asistencias.php
@@ -1,12 +1,12 @@
 <?php
 
-  $host="Localhost";
+    $host="Localhost";
     $usuario="root";
     $password="";
     $db="dbcomedor";
     $idUsuario=$_REQUEST['idUsuario'];
- $c = @mysqli_connect($host, $usuario, $password,$db);
 
+<<<<<<< HEAD
         /* comprueba la conexión */
         if (mysqli_connect_errno()) {
             printf("Connect failed: %s\n", mysqli_connect_error());
@@ -22,30 +22,36 @@
   $datos=array(); 
   $cont=0;
   while($row=mysqli_fetch_row($sql)){
+=======
+    $c = @mysqli_connect($host, $usuario, $password,$db);
+    /* comprueba la conexión */
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
 
-       $fecha=$row[0];
-       $ape_paterno=$row[1];
-       $ape_maerno=$row[2];
-       $nombre=$row[3];
-       $descripcion=$row[4];
-        $objeto= new stdClass();
-       $objeto->fecha=$fecha;
-       $objeto->ape_paterno=$ape_paterno;
-       $objeto->ape_maerno=$ape_maerno;
-       $objeto->nombre=$nombre;
-       $objeto->descripcion=utf8_encode($descripcion);
+    $sql=mysqli_query($c,"SELECT a.fecha,c.ape_paterno,c.ape_maerno,c.nombre,t.descripcion from asistencia a inner join turnos t on a.turno_id=t.id inner join comensales c on a.comensal_id=c.id inner join usuarios u on c.id=u.id_comensal where u.id=".$idUsuario." order by a.fecha");
+>>>>>>> 04883d5a1020b7f25fea68bd2a543e9dd270b8b3
 
-       $datos[]=$objeto;
-       $cont++;
-       
-  }
-  /*
-  $datos[]=array(
-        "fecha"=>"17/06/2015",
-        "ape_paterno"=>"Altamirano",
-        "ape_maerno"=>"Guevara",
-        "nombre"=>"Neyder",
-        "descripcion"=>$idUsuario
-      );*/
-  echo json_encode($datos);
+    $datos=array(); 
+    $cont=0;
+    while($row=mysqli_fetch_row($sql)){
+
+         $fecha=$row[0];
+         $ape_paterno=$row[1];
+         $ape_maerno=$row[2];
+         $nombre=$row[3];
+         $descripcion=$row[4];
+          $objeto= new stdClass();
+         $objeto->fecha=$fecha;
+         $objeto->ape_paterno=$ape_paterno;
+         $objeto->ape_maerno=$ape_maerno;
+         $objeto->nombre=$nombre;
+         $objeto->descripcion=utf8_encode($descripcion);
+
+         $datos[]=$objeto;
+         $cont++;
+         
+    }
+    echo json_encode($datos);
 ?>
